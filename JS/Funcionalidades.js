e6c9b7925892c919cd9eb5b1ee82082dbdff1f62
Departamento de Sistemas <sistemas@AMEGHINO.local>
2023-02-28 16:07:38 -0300

    Proyecto Final CV Claudio Meloni

---
+++ JS/Funcionalidades.js
@@ -0,0 +1,25 @@
+
+document.getElementById('educacion').addEventListener('click', function () {
+    document.getElementById('sdatos').style.color = '#000308';
+    document.getElementById('sperfil').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#F69F2C';
+});
+document.getElementById('perfil').addEventListener('click', function () {
+    document.getElementById('sdatos').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#000308';
+    document.getElementById('sperfil').style.color = '#F69F2C';
+});
+document.getElementById('datos').addEventListener('click', function () {
+    document.getElementById('sperfil').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#000308';
+    document.getElementById('sdatos').style.color = '#F69F2C';
+});
+document.getElementById('experiencia').addEventListener('click', function () {
+    document.getElementById('sdatos').style.color = '#000308';
+    document.getElementById('sperfil').style.color = '#000308';
+    document.getElementById('seducacion').style.color = '#000308';
+    document.getElementById('sexperiencia').style.color = '#F69F2C';
+});
