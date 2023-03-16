28c6429d4c3725e88682ee07b8a38d3addd6ae2d
Departamento de Sistemas <sistemas@AMEGHINO.local>
2023-03-16 17:02:57 -0300

    Proyecto Final CV Claudio Meloni

--- JS/Funcionalidades.js
+++ JS/Funcionalidades.js
@@ -12,10 +12,7 @@ document.getElementById('perfil').addEventListener('click', function () {
     document.getElementById('sperfil').style.color = '#F69F2C';
 });
 document.getElementById('datos').addEventListener('click', function () {
-    document.getElementById('sperfil').style.color = '#000308';
-    document.getElementById('sexperiencia').style.color = '#000308';
-    document.getElementById('seducacion').style.color = '#000308';
-    document.getElementById('sdatos').style.color = '#F69F2C';
+    document.getElementsByClassName('iconos').style.color ='#F69F2C' ;
 });
 document.getElementById('experiencia').addEventListener('click', function () {
     document.getElementById('sdatos').style.color = '#000308';
